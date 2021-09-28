571d3c07f5ac8449ccf268443e910473344ce172
unknown <avinash.sankajji94@gmai.com>
2021-09-28 13:18:01 +0530

    first commit

---
+++ ReversNum/src/com/bridglab/Reversnum/ReversNum.java
@@ -0,0 +1,23 @@
+package com.bridglab.Reversnum;
+
+import java.util.Scanner;
+
+public class ReversNum {
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		int number, reverse = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to reverse it");
+		number = sc.nextInt();
+		// we have not mentioned the initialization part of the for loop
+		for (; number != 0; number = number / 10) 
+		{
+			int remainder = number % 10;
+			reverse = reverse * 10 + remainder;
+		}
+		System.out.println("The reverse of the given number is: " + reverse);
+	}
+
+}
